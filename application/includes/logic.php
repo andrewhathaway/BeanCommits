2ffa2ce55e3a71116143724f475e768ac6cac591
Andrew Hathaway <andrew@andrewhathaway.net>
2012-11-28 22:31:48 +0000

    API request function

--- application/includes/logic.php
+++ application/includes/logic.php
@@ -4,3 +4,6 @@
 	
 	//Required classes
 	$html = Html::getInstance();
+	$beanstalk = Beanstalk::getInstance();
+
+	var_dump($beanstalk->request("GET", "changesets", array('hey' => 'sup', 'aite' => 'boo')));
--- system/classes/beanstalk.php
+++ system/classes/beanstalk.php
@@ -12,6 +12,48 @@ class Beanstalk {
 		return self::$Instance;
 	}
 
-	
+	public function __construct(){
+		$error = Error::getInstance();
+	}
+
+	public function request($type, $endpoint, $data = array()) {
+		global $config, $error;
+		$base = "http://" . $config['account'] . ".beanstalkapp.com/api/";
+
+		$curl = curl_init();
+
+		$options = array(
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_USERPWD => $config['username'] . ":" . $config['password']
+		);
+
+		$url = $base . $endpoint . ".json";	
+
+		if($type == "GET") {
+			$url .= "?" . http_build_query($data);
+		}
+
+		$options[CURLOPT_URL] = $url;
+
+		if($type == "POST") {
+			$options[CUROPT_POSTFIELDS] = $data;
+		}
+
+		foreach($options as $key => $option) {
+			curl_setopt($curl, $key, $option);
+		}
+
+		$response = curl_exec($curl);
+		$json = json_decode($response, true);
+
+		if(isset($json['errors'])) {
+			//Trigger error with message
+			$message = $json['errors'][0];
+			$error->trigger($message);
+		} else {
+			return $json;
+		}
+
+	}
 
 }
