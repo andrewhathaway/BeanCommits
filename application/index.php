69ec04e3c933cd2dd5c967f130a4a10c865c9e70
Andrew Hathaway <andrew@andrewhathaway.net>
2012-12-04 22:11:39 +0000

    Presentation

--- application/index.php
+++ application/index.php
@@ -6,12 +6,18 @@
 <section class="container main-body">
 	<ul class="feed">
 		<?php foreach($feed as $item): ?>
-		<?php $item = $item['revision_cache'] ?>
+		<?php 
+			//Setup item informatiomn
+			$item = $item['revision_cache'];
+			$repository = $repositories[$item['repository_id']];
+			$changed_files = count($item['changed_files']);
+			$time = date("l jS F Y - h:i A",strtotime($item['time']));
+		?>
 			<li>	
 				<img src="<?php echo $html->getGravatar($item['email']); ?>">
 				<div class="information">
 					<h3><?=$item['message']?></h3>
-					<p>In repo <?=$repositories[$item['repository_id']]?> & <?=count($item['changed_files'])?> files changed. <?=date("l jS F Y - h:i A",strtotime($item['time']))?></p>
+					<p>Commit made <?=$changed_files?> changes in repository <?=$repository?> at <?=$time?> - <a href="https://blogcase.beanstalkapp.com/commit-feed/changesets/<?=$item['hash_id']?>">View Commit on Beanstalk</a></p>
 				</div>
 				<div class="clear"></div>
 			</li>
