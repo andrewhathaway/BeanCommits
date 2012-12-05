9d43caf172d41d2a80f71555bd552f69086c03a6
Andrew Hathaway <andrew@andrewhathaway.net>
2012-12-05 22:39:03 +0000

    JS

--- application/js/load-more.js
+++ application/js/load-more.js
@@ -3,6 +3,7 @@ $(function(){
 	var feed = $('#feed');
 	var button = $('#load-more');
 	var href = button.attr('href');
+	var html;
 
 	button.click(function(e){
 		e.preventDefault();
@@ -11,7 +12,8 @@ $(function(){
 		var x = $('<div/>');
 		x.load(href + ' #feed', function(){
 			button.html("LOAD MORE");
-			feed.append(x.html());	
+			html = x.html().replace('<ul class="feed" id="feed">', '').replace('</ul>', '');
+			feed.append(html);	
 
 			var page = href.match(/(\?|&)page=([0-9]+)(&|$)/i)[2];
 			page = parseInt(page, 10) + 1;
