fa4f95c61a1ed136d10cc6a9462c1d5301a01106
Andrew Hathaway <andrew@andrewhathaway.net>
2012-12-08 21:14:39 +0000

    Nicer BASE url setting

--- index.php
+++ index.php
@@ -10,8 +10,7 @@
 	define('APP', ROOT . 'application/');
 	define('INCLUDES', APP . 'includes/');
 	define('SYS', ROOT . 'system/');
-	define('BASE', "http://" . $_SERVER['SERVER_NAME'] . str_replace('index.php', '', $_SERVER['PHP_SELF']));
-	//define('BASE', 'http' . (empty($_SERVER['HTTPS']) ? '' : 's') . '://'. $_SERVER['SERVER_NAME'] . ($_SERVER['SERVER_PORT'] == 80 ? '' : ':' . $_SERVER['SERVER_PORT']) . dirname($_SERVER['REQUEST_URI']) . '/' . (array_key_exists('extension', pathinfo($_SERVER['REQUEST_URI'])) ? '' : basename($_SERVER['REQUEST_URI'])));
+	define('BASE', 'http://' . $_SERVER['SERVER_NAME'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT));
 
 	//Run BeanCommits - http://bukk.it/run.gif
 	include_once(SYS . 'bootstrap.php');
