c97f04367fa1596c9ab6c3227553e35779b50b83
Andrew Hathaway <andrew@andrewhathaway.net>
2012-12-09 19:10:33 +0000

    Revert BASE

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
 	define('APP', ROOT . 'application/');
 	define('INCLUDES', APP . 'includes/');
 	define('SYS', ROOT . 'system/');
-	define('BASE', 'http://' . $_SERVER['SERVER_NAME'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT));
+	//define('BASE', 'http://' . $_SERVER['SERVER_NAME'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT));
+	define('BASE', 'http://' . $_SERVER['SERVER_NAME'] . str_replace('index.php', '', $_SERVER['PHP_SELF']));
 
 	//Run BeanCommits - http://bukk.it/run.gif
 	include_once(SYS . 'bootstrap.php');
