4f67994f7f6e2395d4140eb8d87be84d5a4c9db8
Andrew Hathaway <andrew@andrewhathaway.net>
2012-12-09 16:10:39 +0000

    Add config class

---
+++ system/classes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+class Config {
+	
+	private static $config;
+
+	public static function get() {
+		if(!static::$config) {
+			static::$config = require_once(ROOT . 'config.php');
+		}
+
+		return static::$config;
+	} 
+
+}
